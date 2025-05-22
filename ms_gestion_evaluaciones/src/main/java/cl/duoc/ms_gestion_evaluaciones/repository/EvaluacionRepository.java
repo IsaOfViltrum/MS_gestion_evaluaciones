759c083ddd1d66247c8a098b9fe5fd850b087387
isaOfViltrum <is.sanchezy@duocuc.cl>
2025-05-22 13:59:11 -0400

    conexion y evaluacion service listo y repo

--- ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/repository/EvaluacionRepository.java
+++ ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/repository/EvaluacionRepository.java
@@ -1,5 +1,11 @@
 package cl.duoc.ms_gestion_evaluaciones.repository;
 
-public interface EvaluacionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cl.duoc.ms_gestion_evaluaciones.model.Evaluacion;
 
+@Repository
+public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long> {
+    // JpaRepository ya tiene métodos CRUD básicos
 }
+
--- ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/service/EvaluacionService.java
+++ ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/service/EvaluacionService.java
@@ -1,5 +1,48 @@
 package cl.duoc.ms_gestion_evaluaciones.service;
 
+
+import cl.duoc.ms_gestion_evaluaciones.model.Evaluacion;
+import cl.duoc.ms_gestion_evaluaciones.repository.EvaluacionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EvaluacionService {
 
+    @Autowired
+    private EvaluacionRepository evaluacionRepository;
+
+    public List<Evaluacion> obtenerTodas() {
+        return evaluacionRepository.findAll();
+    }
+
+    public Optional<Evaluacion> obtenerPorId(Long id) {
+        return evaluacionRepository.findById(id);
+    }
+
+    public Evaluacion guardar(Evaluacion evaluacion) {
+        return evaluacionRepository.save(evaluacion);
+    }
+
+    public Evaluacion actualizar(Long id, Evaluacion evaluacionDetalles) {
+        return evaluacionRepository.findById(id).map(evaluacion -> {
+            evaluacion.setNombreEstudiante(evaluacionDetalles.getNombreEstudiante());
+            evaluacion.setTipo(evaluacionDetalles.getTipo());
+            evaluacion.setSeccion(evaluacionDetalles.getSeccion());
+            evaluacion.setMateria(evaluacionDetalles.getMateria());
+            evaluacion.setPeriodo(evaluacionDetalles.getPeriodo());
+            evaluacion.setEstado(evaluacionDetalles.getEstado());
+            evaluacion.setPuntaje(evaluacionDetalles.getPuntaje());
+            evaluacion.setNota(evaluacionDetalles.getNota());
+            return evaluacionRepository.save(evaluacion);
+        }).orElseThrow(() -> new RuntimeException("Evaluacion no encontrada con id " + id));
+    }
+
+    public void eliminar(Long id) {
+        evaluacionRepository.deleteById(id);
+    }
 }
+
