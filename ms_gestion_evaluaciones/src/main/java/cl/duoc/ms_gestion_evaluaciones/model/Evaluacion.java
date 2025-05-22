cea0c28ff35c817e6943b42e26c8a649be2fce57
francis oses <fra.oses@duocuc.cl>
2025-05-22 13:09:19 -0400

    Agregué la entidad Evaluacion para gestionar evaluaciones de estudiantes

--- ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/model/Evaluacion.java
+++ ms_gestion_evaluaciones/src/main/java/cl/duoc/ms_gestion_evaluaciones/model/Evaluacion.java
@@ -5,10 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
